3560bcfc969c0151f34f912abd2a58d66ef01248
sandheep45 <budtut143@gmail.com>
2023-04-23 23:56:45 +0530

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+export default App;
